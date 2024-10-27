aac7d5c4aa2166fe11d12b62d8d8ba2b696c708b
Divyansh <85442869+divyaannsh@users.noreply.github.com>
2024-10-27 19:09:16 +0530

    gh

--- Week1/index.js
+++ Week1/index.js
@@ -1,57 +1,16 @@
-// // cretaeing a http server
-// //express
-// // node default librtary
+import express from 'express'
 
-// const express = require("express");
+const app = express()
 
-// const app = express();
+app.get('/',( req,res) => {
+   res.send('server is ready');
 
-// function sum(n) { m
-//     let ans = 0;
-//     for (let i =1; i<=n; i++){
-//         ans = ans + i;
-//     }
-//   return ans; 
-// }
-// // req is request and res is response
-// app.get("/", function(req, res) {
-//       const n = req.query.n;
-//       const ans = sum(n);  
-//       res.send("hi there ans is "  + ans)
-// })
-     
-// app.listen(3000);
+});
+
+const port = process.env.PORT || 3000;
 
 
-// // function definition
-// function calculateBill() {
-//   // this is the function body
-//   console.log("Running Calculate Bill!!");
-// }
-// // function call or function invocation
-// calculateBill();
-
-
-// // function definition
-// function calculateBill() {
-//   // this is the function body
-//   console.log("Running Calculate Bill!!");
-//   const total = 100 * 1.13;
-//   console.log(total);
-// }
-// // function call or function invocation
-// calculateBill();
-
-// const total = 100 * 1.13;
-// console.log(total);
-// return total;
-
-// // function call or function invocation
-// calculateBill();
-
-function sayHi(name) {
-  function yell() {
-    console.log(name.toUpperCase());
-  }
-  yell();
-}
+app.listen(port,()=> {
+  console.log(`serve at http://localhost:${port  }`);
+  
+})
