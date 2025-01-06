5aa904b3e9e4e88ce4830e7bd23f2284e9fb7b72
Divyansh <85442869+divyaannsh@users.noreply.github.com>
2025-01-06 11:27:58 +0530

    js

---
+++ Jspractice01/script.js
@@ -0,0 +1,29 @@
+//word vs keyword
+
+//chacha = word
+// for = keyword
+//bhaiya = word
+// harshal = word
+// let = keyword
+// const = keyword
+// var = keyword
+// function = keyword
+// return = keyword
+// if = keyword
+// else = keyword
+
+// var, let, const
+// var a = 10;
+// let b = 20;
+// const c = 30;
+// variables and constants
+ 
+//hoitiing 
+// console.log(a);  
+// var a = 10;
+
+// primitives
+// reference
+//aisi koi bhi value jisko copykrne par real copy nahi hoita balki us main value ka refrence pass hojata hai aur jiska refrence pass hua hai us main koi bhi change krne par original value bhi change hojati hai
+// var a = 10;
+//var b = 40;
