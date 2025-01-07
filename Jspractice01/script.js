00787dd713005db0d548e6e9fb0d0d721c5e4666
Divyansh <85442869+divyaannsh@users.noreply.github.com>
2025-01-07 11:22:34 +0530

    Day2

--- Jspractice01/script.js
+++ Jspractice01/script.js
@@ -27,3 +27,36 @@
 //aisi koi bhi value jisko copykrne par real copy nahi hoita balki us main value ka refrence pass hojata hai aur jiska refrence pass hua hai us main koi bhi change krne par original value bhi change hojati hai
 // var a = 10;
 //var b = 40;
+
+//conditionals
+//jab baat agar magar pr ayegi ya fir baat ayegi
+// if(true)
+// {
+//     console.log("Hello");
+// }
+
+// loop for while\
+// 111111111
+//12345676899
+// var a = 1;
+// for (var i = 0; i < 10; i++) {
+//functions mainly teen kaam ke liye hote hai
+//jab aap code chalana chhate ho bar baar with diffrent data 
+
+//function abcd
+   //objects hai ek bande ki details ko hold karna in a key value p[air 
+   //1) blank objects
+   //var a = {}
+//   
+
+// Day 3
+
+// var old js mein tha
+//let const new js mein aya
+ 
+var myVariable = 1222;
+
+//jitne bhi variable ya data hum bananate hai uinhhe store kahi to karna padta hai uske liye yhota ha ia heap meomory
+//execution context jab bhi hum chalyega function apna ek khudka ek imaginaryu  conatiner bana alega jismein uski teem cheeje  hoitiing
+//execution context
+//1) variable object
